d07984edb7fe5fdf8b6fef6e1e8ebea5a9f571b4
Joao Fabris <jvfabris21@gmail.com>
2024-09-12 18:47:22 -0300

    feat:barbershop-item components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,9 +3,12 @@ import Header from "../_components/header";
 import { format } from "date-fns";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-
-const Home = () => {
+const Home = async () => {
+  // chamar prisma e buscar barbearias no banco de dados
+  const barberShops = await db.barbershop.findMany({});
   return (
     <div>
       <Header />
@@ -19,10 +22,23 @@ const Home = () => {
       </div>
       <div className="px-5 mt-6">
         <Search />
+      </div>
 
-        <div className="px-5 mt-6">
-          <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">Agendamento</h2>
-          <BookingItem />
+      <div className="px-5 mt-6">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">
+          Agendamento
+        </h2>
+        <BookingItem />
+      </div>
+      <div className="">
+        <h2 className="text-sm mb-3 uppercase text-gray-400 font-bold">
+          Recomendados
+        </h2>
+        <div>
+          {barberShops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+
+          ))}
         </div>
       </div>
     </div>
